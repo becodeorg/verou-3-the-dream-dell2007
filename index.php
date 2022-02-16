ab83aa800ab0331644158ff4a0405e4c40ece079
Dericioso <dery.lantigua@gmail.com>
2022-02-16 15:00:43 +0100

    Create a button to select currency and change ammount

--- index.php
+++ index.php
@@ -22,17 +22,26 @@
     <h1>Currency Calculator</h1>
 
     <form action="" method="post">
-        <label for="destination">Your destination</label>
+        <select name="currencyOne">
+            <option value="1" id="euro">Euro</option>
+            <!-- <option value="1" id="dollar">Dollar</option> -->
+        </select>
+        <select name="currencyTwo" id="currency">
+            <option value="1.14" name="dollar" id="currency">Dollar</option>
+            <option value="1.58" name="australia" id="currency">AUD</option>
+        </select> <br>
+        <label for="destination">Amount</label>
         <input type="text" name="destination" id="destination">
         <input type="submit">
     </form>
 
     <?php
+
     $amount = $_POST;
     if (empty($amount)) {
         echo "";
     } else {
-        echo $_POST['destination'] * 1.59;;
+        echo $_POST['destination'] * $_POST['currencyTwo'];
     }
 
     ?>
